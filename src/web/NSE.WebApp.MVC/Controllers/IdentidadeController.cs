c30877515b0d808682d3acb4a6c7c1c88dff614d
Jack Wesley <jackwesley@hotmail.com>
2020-05-01 22:37:46 -0300

    Creation of IdentidadeControl Methodes and views and refactoring in _layout and in site.css

--- src/web/NSE.WebApp.MVC/Controllers/IdentidadeController.cs
+++ src/web/NSE.WebApp.MVC/Controllers/IdentidadeController.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using NSE.WebApp.MVC.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -8,5 +9,56 @@ namespace NSE.WebApp.MVC.Controllers
 {
     public class IdentidadeController : Controller
     {
+        [HttpGet]
+        [Route("nova-conta")]
+        public IActionResult Registro()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [Route("nova-conta")]
+        public async Task<IActionResult> Registro(UsuarioRegistro usuarioRegistro)
+        {
+            if (!ModelState.IsValid) return View(usuarioRegistro);
+
+            //Api Registro
+
+            if (false) return View(usuarioRegistro);
+
+            //Realizar Login
+
+            return RedirectToAction("Index", "Home");
+
+        }
+
+        [HttpGet]
+        [Route("login")]
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [Route("login")]
+        public async Task<IActionResult> Login(UsuarioLogin usuarioLogin)
+        {
+            if (!ModelState.IsValid) return View(usuarioLogin);
+
+            //Api Login
+
+            if (false) return View(usuarioLogin);
+
+            //Realizar Login
+
+            return RedirectToAction("Index", "Home");
+        }
+
+        [HttpGet]
+        [Route("sair")]
+        public async Task<IActionResult> Logout()
+        {
+            return RedirectToAction("Index", "Home");
+        }
     }
 }
