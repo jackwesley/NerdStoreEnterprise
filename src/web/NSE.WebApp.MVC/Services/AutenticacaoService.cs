d4bae5eb310c4241612d30c0abdf8b830684d851
Jack Wesley <jackwesley@hotmail.com>
2020-05-04 19:36:29 -0300

    Refactoring using some good practices in code development in HttpServices

--- src/web/NSE.WebApp.MVC/Services/AutenticacaoService.cs
+++ src/web/NSE.WebApp.MVC/Services/AutenticacaoService.cs
@@ -1,4 +1,7 @@
-﻿using NSE.WebApp.MVC.Models;
+﻿using Microsoft.Extensions.Options;
+using NSE.WebApp.MVC.Extensions;
+using NSE.WebApp.MVC.Models;
+using System;
 using System.Net.Http;
 using System.Threading.Tasks;
 
@@ -8,9 +11,12 @@ namespace NSE.WebApp.MVC.Services
     {
         private readonly HttpClient _httpClient;
 
-        public AutenticacaoService(HttpClient httpClient)
+        public AutenticacaoService(HttpClient httpClient, 
+            IOptions<AppSettings> settings)
         {
+            httpClient.BaseAddress = new Uri(settings.Value.AutenticacaoUrl);
             _httpClient = httpClient;
+
         }
 
         public async Task<UsuarioRespostaLogin> Login(UsuarioLogin usuarioLogin)
@@ -18,7 +24,7 @@ namespace NSE.WebApp.MVC.Services
             var loginContent = ObterConteudo(usuarioLogin);
 
             var response = await _httpClient.PostAsync(
-                requestUri: "https://localhost:44307/api/identidade/autenticar",
+                requestUri: "/api/identidade/autenticar",
                 loginContent);
 
             if (!TratarErroResponse(response))
@@ -37,7 +43,7 @@ namespace NSE.WebApp.MVC.Services
             var registroContent = ObterConteudo(usuarioRegistro);
 
             var response = await _httpClient.PostAsync(
-                requestUri: "https://localhost:44307/api/identidade/nova-conta",
+                requestUri: "/api/identidade/nova-conta",
                 registroContent);
 
             if (!TratarErroResponse(response))
--- src/web/NSE.WebApp.MVC/Startup.cs
+++ src/web/NSE.WebApp.MVC/Startup.cs
@@ -2,18 +2,30 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
 using NSE.WebApp.MVC.Configuration;
 
 namespace NSE.WebApp.MVC
 {
     public class Startup
     {
-        public Startup(IConfiguration configuration)
+        public IConfiguration Configuration { get; }
+        public Startup(IHostEnvironment hostEnvironment)
         {
-            Configuration = configuration;
+            var builder = new ConfigurationBuilder()
+                .SetBasePath(hostEnvironment.ContentRootPath)
+                .AddJsonFile(path: "appsettings.json", optional: true, reloadOnChange: true)
+                .AddJsonFile(path: $"appsettings.{hostEnvironment.EnvironmentName}.json", optional: true, reloadOnChange: true)
+                .AddEnvironmentVariables();
+
+            if (hostEnvironment.IsDevelopment())
+            {
+                builder.AddUserSecrets<Startup>();
+            }
+
+            Configuration = builder.Build();
         }
 
-        public IConfiguration Configuration { get; }
 
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
@@ -21,7 +33,7 @@ namespace NSE.WebApp.MVC
 
             services.AddIdentityConfiguration();
 
-            services.AddMvcConfiguration();
+            services.AddMvcConfiguration(Configuration);
 
             services.RegisterServices();
         }
