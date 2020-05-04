a8d0a13327ee2fc81afeeb74cdedb2a068be973a
Jack Wesley <jackwesley@hotmail.com>
2020-05-04 19:06:33 -0300

    Refactoring AutenticacaoService. Creating a Serialize and Deserialize generic service.

--- src/web/NSE.WebApp.MVC/Services/Service.cs
+++ src/web/NSE.WebApp.MVC/Services/Service.cs
@@ -3,12 +3,32 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Net.Http;
+using System.Text;
+using System.Text.Json;
 using System.Threading.Tasks;
 
 namespace NSE.WebApp.MVC.Services
 {
     public abstract class Service
     {
+        protected StringContent ObterConteudo(object dado)
+        {
+            return new StringContent(
+                content: JsonSerializer.Serialize(dado),
+                encoding: Encoding.UTF8,
+                mediaType: "application/json");
+        }
+
+        protected async Task<T> DeserializarObjetoResponse<T>(HttpResponseMessage responseMessage)
+        {
+            var options = new JsonSerializerOptions
+            {
+                PropertyNameCaseInsensitive = true,
+            };
+
+            return JsonSerializer.Deserialize<T>(await responseMessage.Content.ReadAsStringAsync(), options);
+        }
+
         protected bool TratarErroResponse(HttpResponseMessage response)
         {
             switch ((int)response.StatusCode)
