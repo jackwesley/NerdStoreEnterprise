ad5541396bca5d46400cd1b623c7877159461af9
Jack Wesley <jackwesley@hotmail.com>
2020-05-10 21:55:21 -0300

    Creating Acess Validation based on claims

--- src/services/NSE.Catalogo.API/Configuration/SwaggerConfig.cs
+++ src/services/NSE.Catalogo.API/Configuration/SwaggerConfig.cs
@@ -18,6 +18,31 @@ namespace NSE.Catalogo.API.Configuration
                     Contact = new OpenApiContact() { Name = "Jack Wesley Moreira", Email = "jackwesley@hotmail.com" },
                     License = new OpenApiLicense() { Name = "MIT", Url = new Uri(uriString: "https://opensourse.org/licences") }
                 });
+
+                c.AddSecurityDefinition(name: "Bearer", new OpenApiSecurityScheme
+                {
+                    Description = "Insira o token JWT desta maneira: Bearer {seu token}",
+                    Name = "Authorization",
+                    Scheme = "Bearer",
+                    BearerFormat = "JWT",
+                    In = ParameterLocation.Header,
+                    Type = SecuritySchemeType.ApiKey
+                });
+
+                c.AddSecurityRequirement(new OpenApiSecurityRequirement
+                {
+                    {
+                        new OpenApiSecurityScheme
+                        {
+                            Reference = new OpenApiReference
+                            {
+                                Type = ReferenceType.SecurityScheme,
+                                Id = "Bearer"
+                            }
+                        },
+                        new string[] { }
+                    }
+                });
             });
         }
 
