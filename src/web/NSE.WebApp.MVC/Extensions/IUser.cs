1577d7a85dccd1191f9e8db1f2c8933d326d9c3d
Jack Wesley <jackwesley@hotmail.com>
2020-05-11 19:55:37 -0300

    Implementing Delegating Handlers to set token in Catalogo API Call

--- src/web/NSE.WebApp.MVC/Extensions/IUser.cs
+++ src/web/NSE.WebApp.MVC/Extensions/IUser.cs
@@ -11,7 +11,7 @@ namespace NSE.WebApp.MVC.Extensions
         string Name { get; }
         Guid ObterUserId();
         string ObterUserEmail();
-        string ObgerUserToken();
+        string ObterUserToken();
         bool EstaAutenticado();
         bool PossuiRole(string role);
         IEnumerable<Claim> ObterClaims();
@@ -34,7 +34,7 @@ namespace NSE.WebApp.MVC.Extensions
            return EstaAutenticado() ? Guid.Parse(_acessor.HttpContext.User.GetUserId()) : Guid.Empty;
         }
 
-        public string ObgerUserToken()
+        public string ObterUserToken()
         {
             return EstaAutenticado() ? _acessor.HttpContext.User.GetUserToken() : "";
         }
---
+++ src/web/NSE.WebApp.MVC/Services/Handlers/HttpClientAuthorizationDelegatingHandler.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Razor.Language.CodeGeneration;
+using NSE.WebApp.MVC.Extensions;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace NSE.WebApp.MVC.Services.Handlers
+{
+    public class HttpClientAuthorizationDelegatingHandler : DelegatingHandler
+    {
+        private readonly IUser _user;
+        public HttpClientAuthorizationDelegatingHandler(IUser user)
+        {
+            _user = user;
+        }
+
+        protected override Task<HttpResponseMessage> SendAsync(HttpRequestMessage request, CancellationToken cancellationToken)
+        {
+            var authorizationHeader = _user.ObterHttpContext().Request.Headers["Authorization"];
+
+            if (!string.IsNullOrEmpty(authorizationHeader))
+            {
+                request.Headers.Add(name: "Authorization", new List<string>() { authorizationHeader });
+            }
+            
+            var token = _user.ObterUserToken();
+            
+            if (token != null)
+            {
+                request.Headers.Authorization = new AuthenticationHeaderValue(scheme: "Bearer", parameter: token);
+            }
+
+            return base.SendAsync(request, cancellationToken);
+        }
+    }
+}
