a9856ef0df9b60947b4b437510de5c75bcf3d07f
Jack Wesley <jackwesley@hotmail.com>
2020-05-15 19:45:06 -0300

    Creating Models for CLiente API

---
+++ src/services/NSE.Cliente.API/Models/Cliente.cs
@@ -0,0 +1,28 @@
+﻿using NSE.Core.DomainObjects;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace NSE.Cliente.API.Models
+{
+    public class Cliente : Entity, IAggregateRoot
+    {
+        public Cliente(string name, string email, string cpf)
+        {
+            Name = name;
+            Email = email;
+            Cpf = cpf;
+            Excluido = false;
+
+        }
+
+        //Entity Framework(EF) Relation
+        protected Cliente(){}
+
+        public string Name { get; private set; }
+        public string Email { get; private set; }
+        public string Cpf { get; private set; }
+        public bool Excluido { get; private set; }
+        public Endereco Endereco { get; private set; }
+    }
+}
