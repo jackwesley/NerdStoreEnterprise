de3129065c5b591bacd51d9647bcbd92e4018498
Jack Wesley <jackwesley@hotmail.com>
2020-05-15 20:26:26 -0300

    Creating DomainObjects Cpf and Email Improving Client class

--- src/services/NSE.Cliente.API/Models/Cliente.cs
+++ src/services/NSE.Cliente.API/Models/Cliente.cs
@@ -1,28 +1,37 @@
 ﻿using NSE.Core.DomainObjects;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using System;
 
 namespace NSE.Cliente.API.Models
 {
     public class Cliente : Entity, IAggregateRoot
     {
-        public Cliente(string name, string email, string cpf)
+        public Cliente(Guid id, string name, string email, string cpf)
         {
+            Id = id;
             Name = name;
-            Email = email;
-            Cpf = cpf;
+            Email = new Email(email);
+            Cpf = new Cpf(cpf);
             Excluido = false;
 
         }
 
         //Entity Framework(EF) Relation
-        protected Cliente(){}
+        protected Cliente() { }
 
         public string Name { get; private set; }
-        public string Email { get; private set; }
-        public string Cpf { get; private set; }
+        public Email Email { get; private set; }
+        public Cpf Cpf { get; private set; }
         public bool Excluido { get; private set; }
         public Endereco Endereco { get; private set; }
+
+        public void TrocarEmail(string email)
+        {
+            Email = new Email(email);
+        }
+
+        public void AtribuirEndereco(Endereco endereco)
+        {
+            Endereco = endereco;
+        }
     }
 }
