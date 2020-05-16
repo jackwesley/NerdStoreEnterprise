86132a11b0317719c4176e4648c76b0430d30f5a
Jack Wesley <jackwesley@hotmail.com>
2020-05-16 20:19:31 -0300

    Starting implementation of CQRS Creating ClienteCommand and Message

---
+++ src/services/NSE.Cliente.API/Application/Commands/RegistrarClienteCommand.cs
@@ -0,0 +1,28 @@
+﻿using NSE.Core.DomainObjects;
+using NSE.Core.Messages;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace NSE.Cliente.API.Application.Commands
+{
+    public class RegistrarClienteCommand : Command
+    {
+        public Guid Id { get; set; }
+        public string Nome { get; set; }
+        public Email Email { get; set; }
+        public Cpf Cpf { get; set; }
+
+        public RegistrarClienteCommand(Guid id, string nome, Email email, Cpf cpf)
+        {
+            AgregateId = id;
+            Id = id;
+            Nome = nome;
+            Email = email;
+            Cpf = cpf;
+        }
+
+
+    }
+}
