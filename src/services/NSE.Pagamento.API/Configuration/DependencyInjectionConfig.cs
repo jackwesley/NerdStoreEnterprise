ac93846bb82e8d1350d32bd37f7d356be1eeb545
Jack Wesley <jackwesley@hotmail.com>
2020-07-13 20:14:13 -0300

    Configuring Pagamento API and creating NSE.Pagamento.NerdsPag

---
+++ src/services/NSE.Pagamento.API/Configuration/DependencyInjectionConfig.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.DependencyInjection;
+using NSE.Core.Mediator;
+using NSE.WebAPI.Core.Usuario;
+
+
+namespace NSE.Pagamento.API.Configuration
+{
+    public static class DependencyInjectionConfig
+    {
+        public static void RegisterServices(this IServiceCollection services)
+        {
+            //API
+            services.AddSingleton<IHttpContextAccessor, HttpContextAccessor>();
+            services.AddScoped<IAspNetUser, AspNetUser>();
+
+            //Commands
+            
+
+            //Events
+            
+
+
+            //Application
+            services.AddScoped<IMediatorHandler, MediatorHandler>();
+
+        }
+    }
+}
