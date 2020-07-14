a831c1e7065e6897bec86359998b4b9bb1f9abdb
Jack Wesley <jackwesley@hotmail.com>
2020-07-14 20:48:52 -0300

    Facade with payment gatway

---
+++ src/services/NSE.Pagamento.API/Facade/IPagamentoFacade.cs
@@ -0,0 +1,10 @@
+﻿using NSE.Pagamento.API.Models;
+using System.Threading.Tasks;
+
+namespace NSE.Pagamento.API.Facade
+{
+    public interface IPagamentoFacade
+    {
+        Task<Transacao> AutorizarPagamento(NSE.Pagamento.API.Models.Pagamento pagamento);
+    }
+}
---
+++ src/services/NSE.Pagamento.API/Facade/PagamentoConfig.cs
@@ -0,0 +1,9 @@
+﻿
+namespace NSE.Pagamento.API.Facade
+{
+    public class PagamentoConfig
+    {
+        public string DefaultApiKey { get; set; }
+        public string DefaultEncryptionKey { get; set; }
+    }
+}
