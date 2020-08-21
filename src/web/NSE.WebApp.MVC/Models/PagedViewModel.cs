210e0b4ac190c24b9a9e3e99f1e18d7d753f6892
Jack Wesley <jackwesley@hotmail.com>
2020-08-21 08:55:29 -0300

    Creating pagination and view component for CatalogoController

--- src/web/NSE.WebApp.MVC/Models/PagedViewModel.cs
+++ src/web/NSE.WebApp.MVC/Models/PagedViewModel.cs
@@ -4,7 +4,7 @@ using System.Collections.Generic;
 
 namespace NSE.WebApp.MVC.Models
 {
-    public class PagedViewModel<T> where T : class
+    public class PagedViewModel<T> : IPagedList where T : class
     {
         public string ReferenceAction { get; set; }
         public IEnumerable<T> List { get; set; }
@@ -13,5 +13,15 @@ namespace NSE.WebApp.MVC.Models
         public string Query { get; set; }
         public int TotalResults { get; set; }
         public double TotalPages => Math.Ceiling((double)TotalResults / PageSize);
+    }
+
+    public interface IPagedList
+    {
+        public string ReferenceAction { get; set; }
+        public int PageIndex { get; set; }
+        public int PageSize { get; set; }
+        public string Query { get; set; }
+        public int TotalResults { get; set; }
+        public double TotalPages { get; }
     }
 }
