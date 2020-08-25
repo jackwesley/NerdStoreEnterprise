4cac1df61becab0bd2534f3cfa5732ecc84b6998
Jack Wesley <jackwesley@hotmail.com>
2020-08-25 19:38:47 -0300

    Refectoring CarrinhoCliente removing usings

--- src/services/NSE.Carrinho.API/Model/CarrinhoCliente.cs
+++ src/services/NSE.Carrinho.API/Model/CarrinhoCliente.cs
@@ -3,8 +3,7 @@ using FluentValidation.Results;
 using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Security.Cryptography;
-using System.Threading.Tasks;
+
 
 namespace NSE.Carrinho.API.Model
 {
